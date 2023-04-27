e610717fa624dd38c19cec9836f2517ea6bce4a2
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2023-04-27 11:43:08 -0300

    atualizacao

--- src/containers/BlocoCinco/styles.ts
+++ src/containers/BlocoCinco/styles.ts
@@ -3,14 +3,27 @@ import styled from 'styled-components'
 export const Bloco = styled.div`
   background-image: linear-gradient(to right, white 50%, #f4f4f4 50%);
 
+  main {
+    padding: 0 20px;
+  }
+
+  @media (max-width: 768px) {
+    background: #f4f4f4;
+  }
+
   div {
     display: flex;
+    max-width: 1480px;
     justify-content: center;
     column-gap: 45px;
     align-items: center;
 
+    @media (min-width: 1921px) {
+      max-width: 1850px;
+    }
+
     img {
-      height: 100%;
+      width: 100%;
 
       @media (max-width: 768px) {
         width: 100%;
