1eb7ae2fc7ee94d729a59640b072c23c41ccf779
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2023-04-28 11:12:32 -0300

    atualizacao

--- src/components/MenuMobile/index.jsx
+++ src/components/MenuMobile/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from './styles'
 import { FaBars } from 'react-icons/fa'
 import Sidebar from '../Sidebar'
@@ -7,6 +7,10 @@ const MenuMobile = () => {
   const [sidebar, setSidebar] = useState(false)
 
   const showSiderbar = () => setSidebar(!sidebar)
+
+  useEffect(() => {
+    document.body.style.overflowY = showSiderbar ? 'hidden' : 'auto'
+  })
 
   return (
     <Container>
