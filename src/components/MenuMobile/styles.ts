502c8877f5d2494f56dadb7af3bf8ad96d1870e4
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2023-04-28 15:13:55 -0300

    atualizacao

--- src/components/MenuMobile/styles.ts
+++ src/components/MenuMobile/styles.ts
@@ -3,14 +3,16 @@ import styled from 'styled-components'
 export const Container = styled.div`
   > svg {
     position: absolute;
-    top: 28px;
+    top: 30px;
     left: 16px;
     color: #1a202c;
-    height: 35px;
+    height: 27px;
+    width: 27px;
     cursor: pointer;
 
     &:hover {
-      color: rgba(0, 0, 0, 0.6);
+      color: #3b4b6c;
+      // rgb(150, 122, 122)
     }
 
     @media (min-width: 768px) {
